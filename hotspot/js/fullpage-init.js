8c9cd50ab21db2e91c56794acb3b46e100d1e714
Kiki <kikihe030103@gmail.com>
2025-04-27 00:04:40 -0400

    Add files via upload

---
+++ hotspot/js/fullpage-init.js
@@ -0,0 +1,15 @@
+var myFullpage = new fullpage('#fullpage', {
+    navigation: true,
+    
+    // you can also use the HTML attribute data-tooltip on each section instead
+    navigationTooltips: ['First Section', 'Second Section', 'Third Section'],
+    
+    // Navigation for horizontal slides
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom', // Default
+    
+    controlArrows: false,
+    
+    // Get your license at https://alvarotrigo.com/fullPage/pricing
+    licenseKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+}); 
